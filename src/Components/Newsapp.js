a04d3f1aefbf201b3ea78beaaba28bf7082fc634
Mitanshu Patel <mitanshup4@gmail.com>
2024-09-06 00:03:33 +0530

    first commit

--- src/Components/Newsapp.js
+++ src/Components/Newsapp.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import Card from './Card'
-
+import axios from 'axios';
 const Newsapp = () => {
     const [search, setSearch] = useState("india");
     const [newsData, setNewsData] = useState(null)
     const API_KEY = "9c3ed8ee95884dec979460a60f96675b";
 
     const getData = async() =>{
-        const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
+        const response = await axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
         const jsonData = await response.json();
         console.log(jsonData.articles);
         let dt = jsonData?.articles?.slice(0,10)
